9ca585b9ba29c19da297410acc0a3b5b0e196e61
Satendra Pal <109225244+satendra07@users.noreply.github.com>
2023-10-29 19:54:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,63 @@
 
-function onloadPage(){
-    document.getElementById('coupon').style.visibility = 'visible';
-    document.getElementById('body_container').style.opacity = '0.5'
+function onloadPage() {
+  document.getElementById('coupon').style.visibility = 'visible';
+  document.getElementById('body_container').style.opacity = '0.5'
 }
 
-function closeCoupon(){
-    document.getElementById('coupon').style.visibility = 'hidden';
-    document.getElementById('body_container').style.opacity = '1'
+function closeCoupon() {
+  document.getElementById('coupon').style.visibility = 'hidden';
+  document.getElementById('body_container').style.opacity = '1'
 }
 
 
 function darkLight() {
-    var body = document.getElementById('body');
-    var darkLight = document.getElementById('day-night');
-    var nav = document.getElementById('nav_container');
-    var day_night = document.getElementById('day-night');
+  var body = document.getElementById('body');
+  var darkLight = document.getElementById('day-night');
+  var nav = document.getElementById('nav_container');
+  // var day_night = document.getElementsByClassName('fa-moon');
 
-    var nav_a = document.getElementsByClassName('nav_a');
-    Array.from(nav_a).forEach(Element =>{
-        Element.classList.toggle("nav_a-d");
-    })
+  var nav_a = document.getElementsByClassName('nav_a');
+  Array.from(nav_a).forEach(Element => {
+    Element.classList.toggle("nav_a-d");
+  })
 
-    var h1 =document.getElementById('h1')
-    darkLight.classList.toggle("cal_body-light");
-    body.classList.toggle("body-d");
-    nav.classList.toggle("nav_container1");
-    h1.classList.toggle("h1-d");
-    day_night.classList.toggle("fa-sun");
-    // console.log(day_night.[0]);
+  var h1 = document.getElementById('h1')
+  darkLight.classList.toggle("cal_body-light");
+  body.classList.toggle("body-d");
+  nav.classList.toggle("nav_container1");
+  h1.classList.toggle("h1-d");
 
-    // alert("pal")
+  var darkLight1 = darkLight.classList.contains('fa-moon');
+  // var darkLight2 = darkLight.classList.contains('fa-sun');
+  if (darkLight1) {
+    
+    darkLight.classList.remove("fa-moon");
+    darkLight.classList.add("fa-sun");
+    darkLight.style.color="#FFD700";
 
-
- 
-
+  } else {
+    darkLight.classList.remove("fa-sun");
+    darkLight.classList.add("fa-moon");
+    darkLight.style.color="black";
+  }
 
 }
+
+
+
+
 const x = document.getElementById("location");
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
+  } else {
+    alert("Geolocation is not supported by this browser.");
   }
 }
-
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+  alert("Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
 }
+
+ 
